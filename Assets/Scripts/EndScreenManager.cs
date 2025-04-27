85f30da919d948d09837b83914f6e320f763da3b
aoiazure <papyrogaming@gmail.com>
2025-04-27 13:10:48 -0400

    Reverted going to titlescreen, min end screen time

--- Assets/Scripts/EndScreenManager.cs
+++ Assets/Scripts/EndScreenManager.cs
@@ -5,10 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class EndScreenManager : MonoBehaviour
 {
+    private const float timeToWait = 1f;
+    private float timerProgress = 0f;
+
+    private void Start()
+    {
+        timerProgress = 0f;
+    }
+
+    private void Update()
+    {
+        if (timerProgress < timeToWait) timerProgress += Time.deltaTime;
+    }
+
     private void OnMouseDown()
     {
+        if (timerProgress < timeToWait) return;
+
         OrderQueue.OrdersCompleted = 0;
-        //SceneManager.LoadScene("MainGame");
-        SceneManager.LoadScene("TitleScreen");
+        SceneManager.LoadScene("MainGame");
+        //SceneManager.LoadScene("TitleScreen");
     }
 }
