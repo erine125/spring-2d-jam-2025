1f26314399096f1e7926ef9cd156aa6e71fe1fbd
ellio27 <elliotthendricks10@gmail.com>
2025-04-26 23:53:31 -0400

    Moving orders

--- Assets/Scripts/OrderQueue.cs
+++ Assets/Scripts/OrderQueue.cs
@@ -7,7 +7,8 @@ public class OrderQueue : MonoBehaviour
 {
     public static int OrdersCompleted = 0;
 
-    public List<TextMeshProUGUI> OrderLabels;
+    public Canvas ParentCanvas;
+    public GameObject CardPrefab;
 
     public PlantDefinition[] PossiblePlants;
     public int QueueLimit = 4;
@@ -24,21 +25,20 @@ public class OrderQueue : MonoBehaviour
     }
 
     // Not a true queue as only first matching element will be popped, not first element
-    private List<PlantDefinition> queue;
+    private List<Order> queue;
     public int GetQueueLength()
     {
         return queue.Count;
     }
-    public PlantDefinition GetQueueAt(int index)
+    public Order GetQueueAt(int index)
     {
         if (queue.Count > index) return queue[index];
         else return null;
     }
 
-
     public OrderQueue()
     {
-        queue = new List<PlantDefinition>();
+        queue = new List<Order>();
         countdown = 0.01;
     }
 
@@ -48,24 +48,13 @@ public class OrderQueue : MonoBehaviour
         if (countdown <= 0)
         {
             var plantName = AddOrder();
-            if (plantName != "") // if order was added
-            {
-                // TODO: update order ui
-                foreach (var label in OrderLabels)
-                {
-                    if (label != null && label.text == "")
-                    {
-                        // NPC dialogue
-                        var text = npcDialogues[Random.Range(0, npcDialogues.Count)];
-                        string dialogue = text.Replace("{Plant}", $"<color=\"red\">{plantName}</color>");
-                        label.text = dialogue;
-                        break;
-                    }
-                }
-            }
+        }
+
+        foreach (Order order in queue)
+        {
+            order.Update ();
         }
     }
-
 
     // Returns true if the order matched
     public bool FillOrder(string plantName)
@@ -73,10 +62,13 @@ public class OrderQueue : MonoBehaviour
         int found = -1;
         for (int i = 0; i < queue.Count; ++i)
         {
-            if (queue[i].plantName == plantName)
+            if (found != -1)
+            {
+                queue[i].index -= 1; // decrease the index
+            }
+            else if (queue[i].plantDef.plantName == plantName)
             {
                 found = i;
-                break;
             }
         }
 
@@ -85,7 +77,7 @@ public class OrderQueue : MonoBehaviour
             // TODO: display score
             OrdersCompleted += 1;
 
-            OrderLabels[found].text = "";
+            Destroy (queue[found].obj);
             queue.RemoveAt(found);
             return true;
         }
@@ -95,16 +87,79 @@ public class OrderQueue : MonoBehaviour
 
     private string AddOrder()
     {
-        if (GetQueueLength() > QueueLimit) return "";
+        if (GetQueueLength() >= QueueLimit) return "";
 
         PlantDefinition plant = PossiblePlants[Random.Range(0, PossiblePlants.Length - 1)];
-        queue.Add(plant);
+        string npcText = npcDialogues[Random.Range(0, npcDialogues.Count)];
+        string dialogue = npcText.Replace("{Plant}", $"<color=\"red\">{plant.plantName}</color>");
+        int index = queue.Count;
+        GameObject obj = Instantiate (CardPrefab, Vector3.zero, Quaternion.identity);
+        obj.transform.SetParent (ParentCanvas.transform, false);
+
+        queue.Add(new Order(plant, dialogue, index, obj));
         countdown = Random.Range(OrderFrequencyMin, OrderFrequencyMax);
 
-        var text = "Orders: ";
-        foreach (var item in queue) text += $"{item.plantName}, ";
-        Debug.Log(text);
+        var dbgText = "Orders: ";
+        foreach (var item in queue) dbgText += $"{item.plantDef.plantName}, ";
+        Debug.Log(dbgText);
 
         return plant.plantName;
+    }
+}
+
+public class Order
+{
+    public PlantDefinition plantDef;
+    public string text;
+    public int index;
+    public GameObject obj;
+    public RectTransform pos;
+
+    private float CARD_Y_OFFSET = -70; // offset between each card slot
+    private float OFFSCREEN_X = 275;
+    private float SPEED_X = 700;
+    private float SPEED_Y = 500;
+
+    public Order (PlantDefinition plantDef, string text, int index, GameObject obj)
+    {
+        this.plantDef = plantDef;
+        this.index = index;
+        this.obj = obj;
+        pos = obj.GetComponent<RectTransform> ();
+        obj.GetComponentInChildren<TextMeshProUGUI> ().text = text;
+
+        UpdatePos (OFFSCREEN_X, CARD_Y_OFFSET * index);
+    }
+
+    public void Update ()
+    {
+        Vector2 target = GetTargetPos ();
+        Vector2 cur = new Vector2 (pos.anchoredPosition.x, pos.anchoredPosition.y);
+        Vector2 result = Vector2.zero;
+        float dx = target.x - cur.x;
+        float dy = target.y - cur.y;
+
+        float xSpd = SPEED_X * Mathf.Min (1, 0.2f * Mathf.Log (Mathf.Abs(dx) + 4)); // easing func
+        if (Mathf.Abs(dx) <= Time.deltaTime * xSpd) result.x = target.x;
+        else result.x = cur.x + Mathf.Sign (dx) * xSpd * Time.deltaTime;
+
+        float ySpd = SPEED_Y * Mathf.Min (1, 0.2f * Mathf.Log (Mathf.Abs(dy) + 4)); // easing func
+        if (Mathf.Abs(dy) <= Time.deltaTime * ySpd) result.y = target.y;
+        else result.y = cur.y + Mathf.Sign (dy) * ySpd * Time.deltaTime;
+
+        UpdatePos (result.x, result.y);
+    }
+
+    private Vector2 GetTargetPos ()
+    {
+        Vector2 v = Vector2.zero;
+        v.x = 0;
+        v.y = CARD_Y_OFFSET * index;
+        return v;
+    }
+
+    private void UpdatePos (float x, float y)
+    {
+        this.pos.anchoredPosition = new Vector2 (x, y);
     }
 }
